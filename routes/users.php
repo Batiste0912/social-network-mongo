92aa0ac507fac381c888df3ce77bac162ff2bb11
Batiste0912 <batiste.dubo@outlook.fr>
2025-11-08 18:12:35 +0100

    :sparkles: Add most-followed users endpoint with aggregation

--- routes/users.php
+++ routes/users.php
@@ -62,6 +62,69 @@ function user_routes($client): void
             sendResponse(200, ['count' => $total], 'Nombre d\'utilisateurs');
         }
 
+        // Endpoint spécial: /api/users/most-followed -> retourner les utilisateurs les plus suivis
+        if ($id === 'most-followed') {
+            // Récupérer le paramètre limit depuis la query string (par défaut 3)
+            $queryParams = [];
+            parse_str(parse_url($route, PHP_URL_QUERY) ?? '', $queryParams);
+            $limit = isset($queryParams['limit']) ? (int)$queryParams['limit'] : 3;
+
+            // Agrégation sur la collection follows pour compter les followers
+            $follows = $client->social_network->follows;
+
+            $pipeline = [
+                // Grouper par user_follow_id (l'utilisateur suivi) et compter
+                [
+                    '$group' => [
+                        '_id' => '$user_follow_id',
+                        'followerCount' => ['$sum' => 1]
+                    ]
+                ],
+                // Trier par nombre de followers décroissant
+                [
+                    '$sort' => ['followerCount' => -1]
+                ],
+                // Limiter au nombre demandé
+                [
+                    '$limit' => $limit
+                ],
+                // Joindre avec la collection users pour récupérer les informations complètes
+                [
+                    '$lookup' => [
+                        'from' => 'users',
+                        'localField' => '_id',
+                        'foreignField' => '_id',
+                        'as' => 'userInfo'
+                    ]
+                ],
+                // Dérouler le tableau userInfo
+                [
+                    '$unwind' => '$userInfo'
+                ],
+                // Projeter les champs nécessaires
+                [
+                    '$replaceRoot' => [
+                        'newRoot' => [
+                            '$mergeObjects' => [
+                                '$userInfo',
+                                ['followerCount' => '$followerCount']
+                            ]
+                        ]
+                    ]
+                ]
+            ];
+
+            $cursor = $follows->aggregate($pipeline);
+            $results = [];
+            foreach ($cursor as $doc) {
+                $doc['_id'] = (string) $doc['_id'];
+                unset($doc['password']);
+                $results[] = $doc;
+            }
+
+            sendResponse(200, $results, 'Utilisateurs les plus suivis');
+        }
+
         if ($id) {
             try {
                 $objectId = new ObjectId($id);
